97354b888cd6a21b07cd97432e9d03c184aeb76c
Ajay patel <ajaypatelgurjar01@gmail.com>
2025-11-04 23:14:35 +0530

    new

--- select/project/app/models.py
+++ select/project/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField()
+    contact=models.IntegerField()
+    image=models.FileField(upload_to='image/')
+    video=models.FileField(upload_to='viedeo/')
+    audio=models.FileField(upload_to='audio/')
+
+
+
